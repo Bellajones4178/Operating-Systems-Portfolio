d4ad8f33c188e16d42eaf7fa8afd68d0226322f0
Isabella Jones <jonesis@oregonstate.edu>
2025-08-03 00:16:48 +0000

    Added flags ability.

--- smallsh.c
+++ smallsh.c
@@ -1,13 +1,16 @@
-//Sample file used as base//
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define INPUT_LENGTH 2048
 #define MAX_ARGS 512
+
+int last_fg_status = 0;
 
 
 struct command_line
@@ -58,26 +61,29 @@ int main() {
         if (curr_command->argc == 0) {
             continue;
         }
+
         if (strcmp(curr_command->argv[0], "exit") == 0) {
             exit(0);
         } else if (strcmp(curr_command->argv[0], "cd") == 0) {
             char *target_dir = curr_command->argc == 1 ? getenv("HOME") : curr_command->argv[1];
             chdir(target_dir);
+        } else if (strcmp(curr_command->argv[0], "status") == 0) {
+            if (WIFEXITED(last_fg_status)) {
+                printf("exit value %d\n", WEXITSTATUS(last_fg_status));
+            } else if (WIFSIGNALED(last_fg_status)) {
+                printf("terminated by signal %d\n", WTERMSIG(last_fg_status));
+            }
         } else {
-            DIR *currDir = opendir(".");
-            if (curr_command->argc > 0 && strcmp(curr_command->argv[0], "ls") == 0) {
-                if (currDir == NULL) {
-                    perror("Unable to open directory");
-                    exit(EXIT_FAILURE);
-                }
-                struct dirent *entry;
-                while ((entry = readdir(currDir)) != NULL) {
-                    printf("%s\n", entry->d_name);
-                }
-                closedir(currDir);
+            pid_t spawnpid = fork();
+            if (spawnpid == 0) {
+                execvp(curr_command->argv[0], curr_command->argv);
+                perror("execvp");
+                exit(1);
+            } else {
+                waitpid(spawnpid, &last_fg_status, 0);
             }
         }
     }
 
-    return EXIT_SUCCESS;
+    return 0;
 }
